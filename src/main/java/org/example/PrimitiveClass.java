99e908523d39cd7155bed58b7b843aabf70d965c
LilBiscuit <miccichestefano20@gmail.com>
2024-10-28 10:49:30 +0100

    "Añadimos variables"

--- src/main/java/org/example/PrimitiveClass.java
+++ src/main/java/org/example/PrimitiveClass.java
@@ -2,10 +2,12 @@ package org.example;
 
 public class PrimitiveClass {
     public static void main(String[] args) {
-        System.out.println("Hola");
-
-        boolean isActive = true;
-
-        System.out.println(isActive);
+        byte age = 5;
+        short year = 2019;
+        int population = 500000;
+        long distance = 4294967296L;
+        float height = 1.85f;
+        double earthRadius = 6371.0;
+        System.out.println(age);
     }
 }

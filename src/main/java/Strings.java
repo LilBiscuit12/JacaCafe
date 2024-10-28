72e3b0d53bf5a1de526582eed9f74e026bd28d27
LilBiscuit <miccichestefano20@gmail.com>
2024-10-28 11:27:53 +0100

    "Añadimos nombre-"

---
+++ src/main/java/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Stefano";
+        String surname = new String( "Miccichè");
+
+        if (name.equals("Stefano")){
+            System.out.println("The name is Stefano");
+        }
+
+    }
+}

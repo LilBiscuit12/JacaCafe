a3d81a092f2876f304896c68a33ed31fdf7533fd
LilBiscuit <miccichestefano20@gmail.com>
2024-10-28 10:25:56 +0100

    Booleano

--- src/main/java/org/example/PrimitiveClass.java
+++ src/main/java/org/example/PrimitiveClass.java
@@ -4,7 +4,7 @@ public class PrimitiveClass {
     public static void main(String[] args) {
         System.out.println("Hola");
 
-        boolean isActive = false;
+        boolean isActive = true;
 
         System.out.println(isActive);
     }
